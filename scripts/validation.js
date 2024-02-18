f0434f255742c8a4078ebbb1d97dc81500465b0b
JosueR <jrauda912@gmail.com>
2024-02-17 22:09:06 -0800

    refactor: Reassign modal label/input css

--- scripts/validation.js
+++ scripts/validation.js
@@ -3,6 +3,6 @@ const config = {
   inputSelector: ".modal__input",
   submitButtonSelector: ".modal__save-button",
   inactiveButtonClass: ".modal__save-button_disabled",
-  labelErrorClass: ".modal__label_type_error",
+  inputErrorClass: ".modal__input_type_error",
   errorClass: ".modal__error_visible",
 };
